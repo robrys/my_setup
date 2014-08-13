9f091821475e5d29dd87ef2e1e0a311ae78b5150
Dan Trujillo <dtrujillo@appnexus.com>
2014-08-13 14:39:56 -0400

    Updating some things

--- setup.sh
+++ setup.sh
@@ -3,7 +3,25 @@
 # Updating/Initializing all submodules
 git submodule update --init --recursive
 
+
+# Packages
+sudo yum install cmake make gcc gcc-c++ nodejs redis
+sudo npm install forever -g
+
 # Move all files into home directory and load new bash profile
-cp -r . $HOME/
+cp -r .* $HOME/
+cp -r bin $HOME/bin
+
+
+# Installing tmux mem cpu plugin
+pushd $HOME/bin/tmux-mem-cpu-load
+cmake .
+make
+sudo make install
+popd
+
+
+# Setting up bash profile
+sudo cp $HOME/bin/git-prompt.sh /etc/bash_completion.d/git
 source $HOME/.bash_profile
 
